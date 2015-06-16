2b948171019ffc8e8fa0cad9ccab03f8b0fd44bd
xivk <ben@osmsharp.com>
2015-06-16 14:59:53 +0200

    Fixed sample.

--- samples/OsmSharp.Service.Routing.Sample.SelfHost/Program.cs
+++ samples/OsmSharp.Service.Routing.Sample.SelfHost/Program.cs
@@ -1,15 +1,9 @@
-﻿using GTFS;
-using GTFS.IO;
-using Nancy.Hosting.Self;
+﻿using Nancy.Hosting.Self;
 using OsmSharp.Math.Geo.Projections;
 using OsmSharp.Osm.PBF.Streams;
 using OsmSharp.Osm.Streams.Filters;
-using OsmSharp.Osm.Xml.Streams;
 using OsmSharp.Routing;
 using OsmSharp.Routing.Osm.Interpreter;
-using OsmSharp.Routing.Transit.Data;
-using OsmSharp.Routing.Transit.Multimodal.Data;
-using OsmSharp.Routing.Vehicles;
 using OsmSharp.Service.Tiles;
 using OsmSharp.UI.Map.Layers;
 using OsmSharp.UI.Map.Styles.MapCSS;
@@ -36,17 +30,14 @@ namespace OsmSharp.Service.Routing.Sample.SelfHost
                 new OsmSharp.WinForms.UI.Logging.ConsoleTraceListener());
 
             // create router.
-            using (var source = new FileInfo(@"D:\Dropbox\Dropbox\SharpSoftware\Projects\Eurostation ReLive\Server_Dropbox\OSM\relive_kortrijk\kortrijk.osm").OpenRead())
+            using (var source = new FileInfo(@"kempen.osm.pbf").OpenRead())
             {
-                var data = OsmSharp.Routing.Osm.Streams.GraphOsmStreamTarget.Preprocess(
-                    new XmlOsmStreamSource(source), new OsmRoutingInterpreter());
+                var pbfSource = new PBFOsmStreamSource(source);
+                var progress = new OsmStreamFilterProgress();
+                progress.RegisterSource(pbfSource);
+                var router = Router.CreateFrom(progress, new OsmRoutingInterpreter());
 
-                var reader = new GTFSReader<GTFSFeed>();
-                var gtfsFeed = reader.Read<GTFSFeed>(new GTFSDirectorySource(@"D:\Dropbox\Dropbox\SharpSoftware\Projects\Eurostation ReLive\Server_Dropbox\GTFS\relive_kortrijk\delijn_kortrijk_2015_05-06-07"));
-                var connectionsDb = new GTFSConnectionsDb(gtfsFeed);
-                var multimodalConnectionsDb = new MultimodalConnectionsDb(data, connectionsDb, new OsmRoutingInterpreter(), Vehicle.Pedestrian);
-
-                ApiBootstrapper.AddOrUpdate("default", new OsmSharp.Service.Routing.Multimodal.MultimodalRouterWrapperBase(multimodalConnectionsDb));
+                OsmSharp.Service.Routing.ApiBootstrapper.Add("default", router);
             }
 
             // initialize mapcss interpreter.
@@ -54,9 +45,9 @@ namespace OsmSharp.Service.Routing.Sample.SelfHost
                 Assembly.GetExecutingAssembly().GetManifestResourceStream("OsmSharp.Service.Routing.Sample.SelfHost.custom.mapcss"),
                 new MapCSSDictionaryImageSource());
 
-            using (var source = new FileInfo(@"D:\Dropbox\Dropbox\SharpSoftware\Projects\Eurostation ReLive\Server_Dropbox\OSM\relive_kortrijk\kortrijk.osm").OpenRead())
+            using (var source = new FileInfo(@"kempen.osm.pbf").OpenRead())
             {
-                var pbfSource = new XmlOsmStreamSource(source);
+                var pbfSource = new PBFOsmStreamSource(source);
                 var scene = new Scene2D(new OsmSharp.Math.Geo.Projections.WebMercator(), new List<float>(new float[] {
                 16, 14, 12, 10 }));
                 var target = new StyleOsmStreamSceneTarget(
