4732e6b183f67c09ab70e99f43848c29b1f618e1
xivk <ben@osmsharp.com>
2016-04-24 21:04:06 +0200

    It's MIT!

--- Itinero.API.SelfHost/Properties/AssemblyInfo.cs
+++ Itinero.API.SelfHost/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OsmSharp.Routing.API.SelfHost")]
+[assembly: AssemblyTitle("Itinero.API.SelfHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OsmSharp.Routing.API.SelfHost")]
+[assembly: AssemblyProduct("Itinero.API.SelfHost")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
