5fb07b005b1bad554ad67cf84a121d7d4f5759a8
pauldendulk <pauldendulk@gmail.com>
2016-09-09 16:33:11 +0200

    Adding version controller

---
+++ src/Itinero.API/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Itinero.API.Controllers
+{
+    [Route("[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "0.9.0";
+
+        }
+    }
+}
